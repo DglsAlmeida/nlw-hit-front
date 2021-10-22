44b141022a6d76a4f2e2da90a2999a9da6c8e723
Douglas Almeida <douglas.alu.lmb@gmail.com>
2021-10-21 23:27:15 -0300

    add components

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,11 +1,48 @@
 import { LoginBoxLink, LoginBoxTitle, LoginBoxWrapper } from "./styles";
-import { VscGithubInverted } from "react-icons/vsc";
+import { VscGithubInverted, VscMultipleWindows } from "react-icons/vsc";
+import { useEffect } from "react";
+import api from "../../service/api";
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
 
 export const LoginBox = () => {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=f13610a0690c88e44903`;
+
+  const signIn = async (githubCode: string) => {
+    const response = await api.post<AuthResponse>("authenticate", {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem("@NlwHeat:token", token);
+
+    console.log(user);
+  };
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split("?code=");
+      window.history.pushState({}, "", urlWithoutCode);
+      signIn(githubCode);
+    }
+  }, []);
+
   return (
     <LoginBoxWrapper>
       <LoginBoxTitle>Entre e compartilhe sua mensagem</LoginBoxTitle>
-      <LoginBoxLink href="#">
+      <LoginBoxLink href={signInUrl}>
         <VscGithubInverted size={24} />
         Entrar com Github
       </LoginBoxLink>
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,3 +1,55 @@
+import {
+  MessageListWrapper,
+  UserImage,
+  MessageListContainer,
+  Message,
+  MessageUser,
+} from "./styles";
+import logoImg from "../../assets/logo.svg";
+import { useEffect, useState } from "react";
+import api from "../../service/api";
+import { AxiosResponse } from "axios";
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
 export const MessageList = () => {
-  return <div>Message List</div>;
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response: AxiosResponse<Message[], any> = await api.get(
+        "messages/last3"
+      );
+      setMessages(response.data);
+    })();
+  }, []);
+
+  return (
+    <MessageListWrapper>
+      <img src={logoImg} alt="doWhile 2012" />
+
+      <MessageListContainer>
+        {messages.map((message) => (
+          <Message key={message.id}>
+            <div>
+              <p>{message.text}</p>
+              <MessageUser>
+                <UserImage>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </UserImage>
+                <span>{message.user.name}</span>
+              </MessageUser>
+            </div>
+          </Message>
+        ))}
+      </MessageListContainer>
+    </MessageListWrapper>
+  );
 };
